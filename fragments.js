1cceafcbae5d3e6ef676d3d76b9c93ddd45ee934
Palle Cogburn <palle@4thex.com>
2020-08-20 20:02:04 -0400

    Can now go back and forth in the fragments

--- fragments.js
+++ fragments.js
@@ -3,41 +3,117 @@ let actions = [
     kind: "int",
     index: 1,
     action: (svg) => {
-      let w = 50;
-      let h = 50;
-      for (let i = 0; i < 5; i++) {
-        svg
-          .append("rect")
-          .attr("x", i*w)
-          .attr("y", h)
-          .attr("width", w)
-          .attr("height", h)
-          .attr("stroke", "white")
-          .attr("stroke-width", 2)
-      }
+      let interval = 50;
+      let data = Array(8).fill().map((item, index) => {
+        return index * interval;
+      });
+      svg
+        .append("g")
+        .selectAll("line")
+        .data(data)
+        .enter()
+        .append("line")
+        .attr("x1", d => d)
+        .attr("y1", 0)
+        .attr("x2", d => d)
+        .attr("y2", interval*(data.length-1))
+        .attr("stroke", "grey")
+        .attr("stroke-width", 1);
+      svg
+        .append("g")
+        .selectAll("line")
+        .data(data)
+        .enter()
+        .append("line")
+        .attr("x1", 0)
+        .attr("y1", d => d)
+        .attr("x2", interval*(data.length-1))
+        .attr("y2", d => d)
+        .attr("stroke", "grey")
+        .attr("stroke-width", 1);
     }
   },
   {
     kind: "int",
     index: 2,
     action: (svg) => {
+      let interval = 50;
+      let data = [
+        {p: [1, 1, 6, 1], c: "red"},
+        {p: [6, 1, 6, 6], c: "red"},
+        {p: [6, 6, 1, 6], c: "red"},
+        {p: [1, 6, 1, 1], c: "red"},
+        {p: [2, 2, 5, 2], c: "green"},
+        {p: [5, 2, 5, 5], c: "green"},
+        {p: [5, 5, 2, 5], c: "green"},
+        {p: [2, 5, 2, 2], c: "green"},
+        {p: [3, 3, 4, 3], c: "blue"},
+        {p: [4, 3, 4, 4], c: "blue"},
+        {p: [4, 4, 3, 4], c: "blue"},
+        {p: [3, 4, 3, 3], c: "blue"}
+      ];
+
       svg
-        .append("rect")
-        .attr("x", 50)
-        .attr("y", 50)
-        .attr("width", 50)
-        .attr("height", 50)
-        .attr("stroke", "white")
-        .attr("stroke-width", 2)
-        .attr("fill", "white")
+        .append("g")
+        .selectAll("line")
+        .data(data)
+        .enter()
+        .append("line")
+        .attr("x1", d => d.p[0]*interval)
+        .attr("y1", d => d.p[1]*interval)
+        .attr("x2", d => d.p[2]*interval)
+        .attr("y2", d => d.p[3]*interval)
+        .attr("stroke", d => d.c)
+        .attr("stroke-width", 5)
+    }
+  },
+  {
+    kind: "int",
+    index: 3,
+    action: (svg) => {
+      svg.selectAll("*").remove();
+      execute({id: "int-2", svg: svg, previous: true})
+      let interval = 50;
+      let data = [
+        {p: [1, 1, 6, 1], c: "red"},
+        {p: [6, 1, 6, 6], c: "red"},
+        {p: [6, 6, 1, 6], c: "red"},
+        {p: [1, 6, 1, 1], c: "red"},
+        {p: [2, 2, 5, 2], c: "green"},
+        {p: [5, 2, 5, 5], c: "green"},
+        {p: [5, 5, 2, 5], c: "green"},
+        {p: [2, 5, 2, 2], c: "green"},
+        {p: [3, 3, 4, 3], c: "blue"},
+        {p: [4, 3, 4, 4], c: "blue"},
+        {p: [4, 4, 3, 4], c: "blue"},
+        {p: [3, 4, 3, 3], c: "blue"}
+      ];
+
+      // expand the data into single points
+      // let data = data.map(d => {
+      //
+      // });
+      svg
+        .append("g")
+        .selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", d => d.p[0]*interval)
+        .attr("cy", d => d.p[1]*interval)
+        .attr("r", 5)
+        .attr("fill", d => d.c)
     }
   }
-
 ];
 let execute = props => {
   let svg = props.svg;
   let id = props.id;
+  let previous = props.previous;
   let [kind, index] = id.split("-");
+  if(previous) {
+    index--;
+  }
   let todos = actions.filter(action => {
     return action.kind = kind && action.index <= Number(index);
   });
