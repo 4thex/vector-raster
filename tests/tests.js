1d886c73dd874ae5539325f93070aee515d7af6d
Palle Cogburn <palle@4thex.com>
2020-08-30 19:53:55 -0400

    Playing with finding nearest neghbor in a scaled image.

---
+++ tests/tests.js
@@ -0,0 +1,61 @@
+/**
+* @param {Array.<Array.<string>>} points - The 2D array that makes up the original pixels
+* @param {Object} scale - The scale factor of the image
+* @param {Number} scale.x - The horizontal scale
+* @param {Number} scale.y - The vertical scale
+*/
+let NN = scale => {
+  let find = (input) => {
+    let x = Math.round(input.x / scale.x);
+    let y = Math.round(input.y / scale.y);
+    return {x: x, y: y};
+  };
+  return {
+    find: find
+  };
+};
+QUnit.module('NN.find', function() {
+  QUnit.test('Should find nearest neighbor', function(assert) {
+    let nn = NN(
+      // Scaling up from 4 to 5
+      {x: 5/4, y: 5/4}
+    );
+
+    let points = [
+      ["#404040", "#400000", "#004000", "#000040"],
+      ["#808080", "#800000", "#008000", "#000080"],
+      ["#C0C0C0", "#C00000", "#00C000", "#0000C0"],
+      ["#FFFFFF", "#FF0000", "#00FF00", "#0000FF"]
+    ];
+
+    input = {x: 0, y: 0};
+    p = nn.find(input);
+    assert.equal(p.x, 0);
+    assert.equal(p.y, 0);
+    assert.equal(points[p.y][p.x], "#404040");
+
+    input = {x: 1, y: 1};
+    p = nn.find(input);
+    assert.equal(p.x, 1);
+    assert.equal(p.y, 1);
+    assert.equal(points[p.y][p.x], "#800000");
+
+    input = {x: 2, y: 2};
+    p = nn.find(input);
+    assert.equal(p.x, 2);
+    assert.equal(p.y, 2);
+    assert.equal(points[p.y][p.x], "#00C000");
+
+    input = {x: 3, y: 3};
+    p = nn.find(input);
+    assert.equal(p.x, 2);
+    assert.equal(p.y, 2);
+    assert.equal(points[p.y][p.x], "#00C000");
+
+    input = {x: 4, y: 4};
+    p = nn.find(input);
+    assert.equal(p.x, 3);
+    assert.equal(p.y, 3);
+    assert.equal(points[p.y][p.x], "#0000FF");
+  });
+});
