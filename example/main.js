af9ec2bf061a5a9a7baeace6f620b683fe92fceb
Palle Cogburn <palle@4thex.com>
2020-09-01 20:03:02 -0400

    More scaling and destructuring from array.

--- example/main.js
+++ example/main.js
@@ -14,17 +14,41 @@ let originalCtx = canvas.getContext("2d");
 let image = document.querySelector("#image");
 image.src = "image.jpg";
 let originalImage;
-image.addEventListener("load", event => {
-  originalCtx.drawImage(image, -900, -500);
-  originalImage = originalCtx.getImageData(0, 0, canvas.width, canvas.height);
-});
 let scaled = document.querySelector("#scaled");
 let scaledCtx = scaled.getContext("2d");
 let scaledImage = scaledCtx.createImageData(scaled.width, scaled.height);
-let nn = NN({
-  x: scaled.width/canvas.width,
-  y: scaled.height/canvas.height
+image.addEventListener("load", event => {
+  originalCtx.drawImage(image, -900, -500);
+  originalImage = originalCtx.getImageData(0, 0, canvas.width, canvas.height);
+  let pixels = Pixels(originalImage);
+  let pixel = pixels.x(0).y(0);
+  let nn = NN({
+    x: scaled.width/canvas.width,
+    y: scaled.height/canvas.height
+  });
+
+  let nn1 = nn.find({x:, y:0});
+  let t1 = pixels.x(nn1.x).y(nn1.y);
 });
-scaledImage.data.forEach((item, i) => {
-  
-});
+
+// let Scale = (originalImageData, nn)
+
+let Pixels = imageData => {
+  let pixel = (x, y) => {
+    let index = imageData.width*4*y+x*4;
+    let p = {};
+    [p.r, p.g, p.b, p.a] = imageData.data.slice(index, index+4);
+    return p;
+  };
+  let x = x => {
+    let y = y => {
+      return pixel(x, y);
+    };
+    return {
+      y: y
+    };
+  };
+  return {
+    x: x
+  };
+};
